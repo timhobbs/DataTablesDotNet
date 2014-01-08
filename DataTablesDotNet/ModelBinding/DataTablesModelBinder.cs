634418ea3da6c190eb072b288039b7acccdbffbf
Tim Hobbs <jesus.tesh@gmail.com>
2014-01-08 07:03:47 -0800

    Model binder updates
    Changed from using QS to Params (allows for post, etc.)
    Merged multliple loops - what was I thinking? :)

--- DataTablesDotNet/ModelBinding/DataTablesModelBinder.cs
+++ DataTablesDotNet/ModelBinding/DataTablesModelBinder.cs
@@ -11,42 +11,31 @@ namespace DataTablesDotNet.ModelBinding {
             HttpRequestBase request = controllerContext.HttpContext.Request;
             var model = new DataTablesRequest();
             if (request.QueryString.Count > 0) {
-                var qs = request.QueryString;
+                var requestParams = request.Params;
                 var properties = model.GetType().GetProperties();
 
                 // This will loop through the primitive properties and set the values
                 foreach (var property in properties) {
                     var value = property.GetValue(model, null);
                     if (property.PropertyType.IsInterface == false) {
-                        var formValue = qs.Get(property.Name);
+                        var formValue = requestParams.Get(property.Name);
                         property.SetValue(model, Convert.ChangeType(formValue, property.PropertyType), null);
                     }
                 }
 
                 // Fill lists
                 for (int i = 0; i < model.iColumns; i++) {
-                    model.sSearchList.Add(qs.Get("sSearch_" + i));
+                    model.sSearchList.Add(requestParams.Get("sSearch_" + i));
+                    model.mDataProp.Add(requestParams.Get("mDataProp_" + i));
+                    model.bRegexList.Add(Convert.ToBoolean(requestParams.Get("bRegex_" + i) ?? "false"));
+                    model.bSearchable.Add(Convert.ToBoolean(requestParams.Get("bSearchable_" + i) ?? "false"));
+                    model.bSortable.Add(Convert.ToBoolean(requestParams.Get("bSortable_" + i) ?? "false"));
                 }
 
-                for (int i = 0; i < model.iColumns; i++) {
-                    model.mDataProp.Add(qs.Get("mDataProp_" + i));
-                }
-
-                for (int i = 0; i < model.iColumns; i++) {
-                    model.bRegexList.Add(Convert.ToBoolean(qs.Get("bRegex_" + i) ?? "false"));
-                }
-
-                for (int i = 0; i < model.iColumns; i++) {
-                    model.bSearchable.Add(Convert.ToBoolean(qs.Get("bSearchable_" + i) ?? "false"));
-                }
-
-                for (int i = 0; i < model.iColumns; i++) {
-                    model.bSortable.Add(Convert.ToBoolean(qs.Get("bSortable_" + i) ?? "false"));
-                }
-
+                // Use iSortingCols to fill sort-related lists
                 for (int i = 0; i < model.iSortingCols; i++) {
-                    model.iSortCol.Add(qs.Get("iSortCol_" + i));
-                    model.sSortDir.Add(qs.Get("sSortDir_" + i));
+                    model.iSortCol.Add(requestParams.Get("iSortCol_" + i));
+                    model.sSortDir.Add(requestParams.Get("sSortDir_" + i));
                 }
             }
 
